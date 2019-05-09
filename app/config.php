3cfe09f86b949b35a710290a2b7458a482d19bb6
Mark N <mark.is.at.a.computer@gmail.com>
2019-05-09 07:24:23 -0700

    app - custom Dockerfile and config.php

---
+++ app/config.php
@@ -0,0 +1,51 @@
+<?php
+
+$CONFIG = array(
+  //|  DOMAINS
+  'trusted_domains' =>
+    array (
+      'nextcloud.local',
+      $_ENV['VIRTUAL_HOST'],
+      '127.*.*.*'
+    ),
+  //|  DB
+  'dbtype' => 'pgsql',
+  'dbhost' => $_ENV['POSTGRES_HOST'],
+  'dbname' => $_ENV['POSTGRES_DATABASE'],
+  'dbuser' => $_ENV['POSTGRES_USER'],
+  'dbpassword' => $_ENV['POSTGRES_PASSWORD'],
+  //|  use token for clients
+  'token_auth_enforced' => true,
+  //|  proxy settings
+  //'overwriteprotocol' => 'https',
+  //'overwrite.cli.url' => 'https://' . $_ENV['VIRTUAL_HOST'] . '/',
+  //'htaccess.RewriteBase' => '/',
+  //|  APPS
+  'apps_paths' => array(
+    array(
+      'path'=> '/usr/src/nextcloud/apps',
+      'url' => '/apps',
+      'writable' => true,
+    ),
+    array(
+      'path'=> '/usr/src/nextcloud/cutom_apps',
+      'url' => '/custom_apps',
+      'writable' => false,
+    ),
+  ),
+  //|  STORE
+  'objectstore' => array(
+    'class' => 'OC\\Files\\ObjectStore\\S3',
+    'arguments' => array(
+      'autocreate' => true,
+      'bucket' => $_ENV['S3_BUCKET'],
+      'key'    => $_ENV['S3_KEY'],
+      'secret' => $_ENV['S3_SECRET'],
+      'hostname' => $_ENV['S3_HOSTNAME'],
+      'port' => $_ENV['S3_PORT'],
+      'use_ssl' => true,
+      'region' => $_ENV['S3_REGION'],
+      'use_path_style'=>true
+    ),
+  ),
+);
